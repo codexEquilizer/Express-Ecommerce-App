61a229de498b267099f407697ffb0cacf03cff1c
Sourav Dutta <sourav.equilizer007@gmail.com>
2026-03-20 02:01:48 +0530

    Using magic association method

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,14 +13,16 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
-    title: title,
-    imageUrl: imageUrl,
-    price: price,
-    description: description,
-  })
+  req.user
+    .createProduct({
+      title: title,
+      imageUrl: imageUrl,
+      price: price,
+      description: description,
+    })
     .then((result) => {
-      (console.log(result), res.redirect("/admin/products"));
+      console.log("Created Product");
+      res.redirect("/admin/products");
     })
     .catch((err) => console.log(err));
 };

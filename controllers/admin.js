dfb569aaa737c5266fac6725c62ac0c9dce8f436
Sourav Dutta <sourav.equilizer007@gmail.com>
2026-03-20 00:45:58 +0530

    Deleting products

--- controllers/admin.js
+++ controllers/admin.js
@@ -19,7 +19,9 @@ exports.postAddProduct = (req, res, next) => {
     price: price,
     description: description,
   })
-    .then((result) => console.log(result))
+    .then((result) => {
+      (console.log(result), res.redirect("/admin/products"));
+    })
     .catch((err) => console.log(err));
 };
 
@@ -79,6 +81,13 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId);
-  res.redirect("/admin/products");
+  Product.findByPk(prodId)
+    .then((product) => {
+      return product.destroy();
+    })
+    .then((result) => {
+      console.log("PRODUCT DESTROYED");
+      res.redirect("/admin/products");
+    })
+    .catch((err) => console.log(err));
 };
